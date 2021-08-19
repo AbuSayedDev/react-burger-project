25305961b0529acd99fa6c7fcd4f271e0cda3e20
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-08-20 01:19:19 +0600

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
 
 const Header = () => {
     return(
         <div>
-            <h1>Header</h1>
+            <Navbar style={{ backgroundColor: "#D70F64", height: "70px"}}>
+
+                <NavbarBrand href="/" className="mr-auto ml-md-5">Brand</NavbarBrand>
+
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink href="#">Something</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
         </div>
     )
 }
