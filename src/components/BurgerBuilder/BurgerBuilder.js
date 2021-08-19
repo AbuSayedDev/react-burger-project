bb9afb9e084776ded866f111a3f589bedff03f9c
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-08-20 00:52:58 +0600

    project setup

---
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class BurgerBuilder extends Component {
+    render(){
+        return(
+            <div>
+                <p>Burger Builder </p>
+            </div>
+        )
+    }
+
+}
+
+export default BurgerBuilder;
