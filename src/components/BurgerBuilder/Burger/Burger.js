63a47079c3cc99b407f9f9204dc8615fcb2631db
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-08-20 22:02:23 +0600

    ingredient state integrations

--- src/components/BurgerBuilder/Burger/Burger.js
+++ src/components/BurgerBuilder/Burger/Burger.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Ingredient from '../Ingredient/Ingredient';
+import './Burger.css';
 
 const Burger = props =>{
+
+    let ingredientArr = props.ingredients.map(item => {
+    let amountArr = [...Array(item.amount).keys()]
+        return amountArr.map(_ => {
+            return <Ingredient  type={item.type} key={Math.random()}/>
+        });
+    })
+    .reduce((arr, element) => {
+        return arr.concat(element)
+    }, []);
+
+    if(ingredientArr.length === 0){
+        ingredientArr = <p className="my-2 p-0">Please add some Ingredients ! </p>
+    }
+
+
     return(
-        <div>
+        <div className="Burger">
             <Ingredient  type="bread-top"/>
-            <Ingredient  type="cheese"/>
-            <Ingredient  type="salad"/>
-            <Ingredient  type="meat"/>
+            {ingredientArr}
             <Ingredient  type="bread-bottom"/>
         </div>
     )
