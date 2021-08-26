2db5e92eff037e9006562e96d4232fa004e6a164
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-08-26 11:06:36 +0600

    redux integrations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import reactDom from 'react-dom';
-import './App.css';
-import Main from './components/Main.js';
+import React from "react";
+import reactDom from "react-dom";
+import "./App.css";
+import Main from "./components/Main.js";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+
+
+
 
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Main />
+                </BrowserRouter>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
